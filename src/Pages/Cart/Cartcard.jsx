75d3e7ea0adacde4a615ddd5c3c4bc3c25e0d166
biyash <bsstha21@gmail.com>
2024-05-17 19:52:24 +0545

    state maging added

--- src/Pages/Cart/Cartcard.jsx
+++ src/Pages/Cart/Cartcard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import { useDispatch} from 'react-redux'
+import {removeFromCart} from '../../features/cartSlice'
 const Cartcard = ({product}) => {
+
+  const dispatch = useDispatch();
   return (
     <div className='flex flex-wrap justify-between border-b dark:border-slate-700 max-w-4xl m-auto p-2 mb-5 '>
       <div className='flex'>
   <img src={product.image} className = "h-28 w-28 " />
      <div>
         <h1 className='p-2 text-xl'>{product.name}</h1>
-        <button className='text-base ml-2 text-red-400'>remove</button>
+        <button className='text-base ml-2 text-red-400' onClick={() => dispatch(removeFromCart(product))}>remove</button>
      </div>
       </div>
    <div>
