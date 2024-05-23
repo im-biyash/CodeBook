727e579567e6cfa66ac5fd3ba1ae971e7dde0185
biyash <bsstha21@gmail.com>
2024-05-23 16:01:08 +0545

    protected route

---
+++ src/Pages/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+    const token = JSON.parse(sessionStorage.getItem('token'));
+    return token ? children : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
