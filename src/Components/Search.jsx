2bc6b6c295edde8f20087f6be236136f37bfcebe
biyash <bsstha21@gmail.com>
2024-05-23 23:09:32 +0545

    search box opens

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,25 +1,26 @@
-import React  from 'react';
+import React from 'react';
 import { IoMdSearch } from "react-icons/io";
 
 const Search = () => {
-   
-    return (
-        <div className='mx-auto max-w-[50%]'>
-            <form onSubmit={handleSubmit} className='flex items-center'>
-                <div className='relative w-full'>
-                    <span>
-                        <IoMdSearch className='absolute top-1/2 left-2 text-black -translate-y-1/2 text-gray-400' />
-                    </span>
-                    <input
-                        type="text"
-                        
-                        className='w-full p-2 rounded-l-lg bg-gray-50 border'
-                    />
-                </div>
-                <button type='submit' className='p-2 ml-2 bg-green-400 rounded'>Search</button>
-            </form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle the search submit logic here
+  };
+
+  return (
+    <div className='mx-auto max-w-[50%] mt-4'> {/* Added margin-top for better positioning */}
+      <form onSubmit={handleSubmit} className='flex items-center'>
+        <div className='relative w-full'>
+          <IoMdSearch className='absolute top-1/2 left-2 text-black -translate-y-1/2 text-gray-400' />
+          <input
+            type="text"
+            className='w-full p-2 pl-10 rounded-l-lg bg-gray-50 border' // Adjusted padding-left for better icon spacing
+          />
         </div>
-    );
-}
+        <button type='submit' className='p-2 ml-2 bg-green-400 rounded-r-lg'>Search</button> {/* Added rounded-right */}
+      </form>
+    </div>
+  );
+};
 
 export default Search;
